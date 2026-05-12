28d96025ba0b30683c9bf42cacf1fe3ba176b82c
aniki-16x16 <aniki_16x16@outlook.com>
2026-05-13 02:03:36 +0800

    chore: apk构建 & github actions

---
+++ android/app/src/main/java/com/qz1ii/nineblockpractice/MainActivity.java
@@ -0,0 +1,5 @@
+package com.qz1ii.nineblockpractice;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.qz1ii.nineblockpractice',
+  appName: '九宫格练习',
+  webDir: 'dist'
+};
+
+export default config;
